01a07ed47ccb3ce6e67415f82436163ab71bb4a0
Daiana <daianaroxys@gmail.com>
2023-06-29 17:43:35 +0200

    instalacion de body-parser, creacion del entorno de pruebas y conexcion al formulario

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const {urlencoded} = require('body-parser');
 
 const server = express();
 
-server.get('/',(req,res) => res.send('holi'));
+server.use(urlencoded({extended:true}));
+server.use('/prueba', express.static('prueba'));
+
+server.post('/nuevo-producto', (req, res) => {
+    res.send(req.body);
+});
 
 server.listen(4000);
