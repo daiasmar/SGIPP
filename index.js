787a8bb3988c03122e20f491309a354c37e117bd
Daiana <daianaroxys@gmail.com>
2023-06-29 18:21:01 +0200

    el login se ha empezado

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const {urlencoded} = require('body-parser');
+const session = require('express-session');
+
+let usuarios = [
+    { usuario : 'Daiana', contraseña : 'root' },
+    { usuario : 'Evaluador', contraseña : '1234' },
+    { usuario : 'Trambolico', contraseña : 'SubiryBajar' }
+]
 
 const server = express();
 
+server.use(session({
+    secret : '1234',
+    resave : true,
+    saveUninitialized : false
+}));
+
 server.use(urlencoded({extended:true}));
-server.use('/prueba', express.static('prueba'));
+
+
+server.use('/login', express.static('login'));
+server.use('/', express.static('prueba'));
+
+server.get('/', (req, res) => {
+    if(req.session.usuarios){
+        return res.send('holi');
+    }
+    res.redirect('/login');
+})
 
 server.post('/nuevo-producto', (req, res) => {
     res.send(req.body);
